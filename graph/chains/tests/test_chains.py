bc19dd35f62ddbcde6888c11cf929f982bf26eb6
oscarmoreno219 <oscar.moreno@synergialabs.com.ec>
2025-09-30 13:31:38 -0500

    feat: state, chains, grade

--- graph/chains/tests/test_chains.py
+++ graph/chains/tests/test_chains.py
@@ -1,2 +1,29 @@
-def test_foo() -> None:
-    assert 1 == 1
+from ingestion import retriever
+from graph.chains.retrieval_grader import GradeDocuments, retrieval_grader
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+def test_retrival_grader_answer_yes() -> None:
+    question = "agent memory"
+    docs = retriever.invoke(question)
+    doc_txt = docs[1].page_content
+
+    res: GradeDocuments = retrieval_grader.invoke(
+        {"question": question, "document": doc_txt}
+    )
+
+    assert res.binary_score == "yes"
+
+
+def test_retrival_grader_answer_no() -> None:
+    question = "agent memory"
+    docs = retriever.invoke(question)
+    doc_txt = docs[1].page_content
+
+    res: GradeDocuments = retrieval_grader.invoke(
+        {"question": "how to make pizaa", "document": doc_txt}
+    )
+
+    assert res.binary_score == "no"
