9eeb7db7e22fb82f6e7d38049278d8a86498968a
oscarmoreno219 <oscar.moreno@synergialabs.com.ec>
2025-10-02 18:38:04 -0500

    feat: self rag with hallucinations grader

--- graph/chains/tests/test_chains.py
+++ graph/chains/tests/test_chains.py
@@ -3,6 +3,9 @@ from graph.chains.retrieval_grader import GradeDocuments, retrieval_grader
 from dotenv import load_dotenv
 
 from graph.chains.generation import generation_chain
+from graph.chains.hallucination_grader import hallucination_grader, GradeHallucinations
+
+
 
 load_dotenv()
 
@@ -38,3 +41,28 @@ def test_generation_chain() -> None:
     generation = generation_chain.invoke(
         {"context": docs, "question": question})
     print(generation)
+
+
+def test_hallucination_grader_answer_yes() -> None:
+    question = "agent memory"
+    docs = retriever.invoke(question)
+
+    generation = generation_chain.invoke(
+        {"context": docs, "question": question})
+    res: GradeHallucinations = hallucination_grader.invoke(
+        {"documents": docs, "generation": generation}
+    )
+    assert res.binary_score
+
+
+def test_hallucination_grader_answer_no() -> None:
+    question = "agent memory"
+    docs = retriever.invoke(question)
+
+    res: GradeHallucinations = hallucination_grader.invoke(
+        {
+            "documents": docs,
+            "generation": "In order to make pizza we need to first start with the dough",
+        }
+    )
+    assert not res.binary_score
